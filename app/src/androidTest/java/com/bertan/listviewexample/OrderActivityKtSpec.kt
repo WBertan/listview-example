12f1aeeeaa925e3d261ff08ef1948448921634da
WBertan <william.bertan@gmail.com>
2019-01-25 13:11:22 +0000

    Add example of running multiple cases and catching the assertion failure to return a list of errors cases at the end.

--- app/src/androidTest/java/com/bertan/listviewexample/OrderActivityKtSpec.kt
+++ app/src/androidTest/java/com/bertan/listviewexample/OrderActivityKtSpec.kt
@@ -6,6 +6,7 @@ import android.support.test.espresso.matcher.ViewMatchers.*
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import org.hamcrest.Matchers.*
+import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -64,5 +65,74 @@ class OrderActivityKtSpec {
             .atPosition(0)
             .check(matches(hasDescendant(allOf(withId(R.id.textViewTotalItems), withText(expectedItemsTotal)))))
             .check(matches(hasDescendant(allOf(withId(R.id.textViewTotal), withText(expectedTotal)))))
+    }
+
+    @Test
+    fun multiple_failing_tests_on_one_go() {
+        val case01 = {
+            val item01 = OrderActivity.Item("dummyItem01", 1.0)
+            val item02 = OrderActivity.Item("dummyItem02", 2.0)
+            val order = OrderActivity.Order(listOf(item01, item02))
+
+            val expectedItemsTotal = "9999"
+            val expectedTotal = "9999.0"
+
+            rule.activity.updateListView(listOf(order))
+
+            onData(`is`(instanceOf(OrderActivity.Order::class.java)))
+                .inAdapterView(withId(R.id.listView))
+                .atPosition(0)
+                .check(matches(hasDescendant(allOf(withId(R.id.textViewTotalItems), withText(expectedItemsTotal)))))
+                .check(matches(hasDescendant(allOf(withId(R.id.textViewTotal), withText(expectedTotal)))))
+        }
+        val case02 = {
+            val item01 = OrderActivity.Item("dummyItem01", 1.0)
+            val item02 = OrderActivity.Item("dummyItem02", 2.0)
+            val order = OrderActivity.Order(listOf(item01, item02))
+
+            val expectedItemsTotal = "2"
+            val expectedTotal = "3.0"
+
+            rule.activity.updateListView(listOf(order))
+
+            onData(`is`(instanceOf(OrderActivity.Order::class.java)))
+                .inAdapterView(withId(R.id.listView))
+                .atPosition(0)
+                .check(matches(hasDescendant(allOf(withId(R.id.textViewTotalItems), withText(expectedItemsTotal)))))
+                .check(matches(hasDescendant(allOf(withId(R.id.textViewTotal), withText(expectedTotal)))))
+        }
+        val case03 = {
+            val item01 = OrderActivity.Item("dummyItem01", 1.0)
+            val item02 = OrderActivity.Item("dummyItem02", 2.0)
+            val order = OrderActivity.Order(listOf(item01, item02))
+
+            val expectedItemsTotal = "8888"
+            val expectedTotal = "8888.0"
+
+            rule.activity.updateListView(listOf(order))
+
+            onData(`is`(instanceOf(OrderActivity.Order::class.java)))
+                .inAdapterView(withId(R.id.listView))
+                .atPosition(0)
+                .check(matches(hasDescendant(allOf(withId(R.id.textViewTotalItems), withText(expectedItemsTotal)))))
+                .check(matches(hasDescendant(allOf(withId(R.id.textViewTotal), withText(expectedTotal)))))
+        }
+        //Expect Case01/Case03 fail and Case02 pass
+        val expectedErrors = listOf(
+            "case01",
+            "case03"
+        )
+        val listErrors = mutableListOf<String>()
+        try {
+            case01()
+        } catch (exception: AssertionError) { listErrors.add("case01") }
+        try {
+            case02()
+        } catch (exception: AssertionError) { listErrors.add("case02") }
+        try {
+            case03()
+        } catch (exception: AssertionError) { listErrors.add("case03") }
+
+        assertEquals(expectedErrors, listErrors)
     }
 }
